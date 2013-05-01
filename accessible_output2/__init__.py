82601c7f34b70bd9524431971c1d0812c9cf2743
Tyler Spivey <tspivey@pcdesk.net>
2013-04-30 20:31:53 -0700

    Add find_datafiles

--- accessible_output2/__init__.py
+++ accessible_output2/__init__.py
@@ -12,3 +12,15 @@ def get_output_classes():
  module_type = types.ModuleType
  classes = [m.output_class for m in outputs.__dict__.itervalues() if type(m) == module_type and hasattr(m, 'output_class')]
  return sorted(classes, key=lambda c: c.priority)
+
+def find_datafiles():
+ import os
+ import platform
+ from glob import glob
+ import accessible_output2
+ if platform.system() != 'Windows':
+  return []
+ path = os.path.join(accessible_output2.__path__[0], 'lib', '*.dll')
+ results = glob(path)
+ dest_dir = os.path.join('accessible_output2', 'lib')
+ return [(dest_dir, results)]
