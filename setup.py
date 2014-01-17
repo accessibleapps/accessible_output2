db38217a722ae9de4e3a567ee9b7ca76ec4254c6
Q <q@q-continuum.net>
2014-01-16 23:39:06 -0700

    Preparing release 0.11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
  name = 'accessible_output2',
  author = 'Tyler Spivey',
  author_email = 'tspivey@pcdesk.net',
- version = '0.1',
+ version='0.11',
  description = __doc__,
  long_description = long_description,
  package_dir = {'accessible_output2': 'accessible_output2'},
