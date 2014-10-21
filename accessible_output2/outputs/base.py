894f4e43894f4ea6c3d1f5504890dc16fa469ef4
Q <q@q-continuum.net>
2014-10-21 14:02:49 -0600

    Added modules for VoiceOver and Apple Say. They both thread using standard command line ?&? forking. Chris Norman <c.c.norman@me.com>

--- accessible_output2/outputs/base.py
+++ accessible_output2/outputs/base.py
@@ -5,26 +5,26 @@ class OutputError(Exception):
  pass
 
 class Output(object):
- name = "Unnamed Output" #The name of this output
- lib32 = None #name of 32-bit lib
- lib64 = None #name of 64-bit lib
- priority = 100 #Where to sort in the list of available outputs for automaticly speaking
+ name = "Unnamed Output"
+ lib32 = None
+ lib64 = None
+ priority = 100
  system_output = False
 
  def __init__(self):
-  is_32bit = platform.architecture()[0] == "32bit"
-  if self.lib32 and is_32bit:
+  self.is_32bit = platform.architecture()[0] == "32bit"
+  if self.lib32 and self.is_32bit:
    self.lib = load_library(self.lib32)
   elif self.lib64:
    self.lib = load_library(self.lib64)
+  else:
+   self.lib = None
 
  def output(self, text, **options):
   output = False
-  if hasattr(self, 'speak') and callable(self.speak):
-   self.speak(text, **options)
+  if self.speak(text, **options):
    output = True
-  if hasattr(self, 'braille') and callable(self.braille):
-   self.braille(text, **options)
+  if self.braille(text, **options):
    output = True
   if not output:
    raise RuntimeError("Output %r does not have any method defined to output" % self)
@@ -32,3 +32,8 @@ class Output(object):
  def is_system_output(self):
   return self.system_output
 
+ def speak(self, **optiont):
+  return False
+
+ def braille(self, **options):
+  return False
--- accessible_output2/outputs/voiceover.py
+++ accessible_output2/outputs/voiceover.py
@@ -1,22 +1,1 @@
-import subprocess, threading
-from base import Output
-
-class VoiceOver(Output):
- """Speech output supporting the Apple VoiceOver screen reader."""
- def runAppleScript(self, command, process = 'voiceover'):
-  return subprocess.Popen(['osascript', '-e', 'tell application "' + process + '"\n' + command + '\nend tell'], stdout = subprocess.PIPE).communicate()[0]
- name = 'VoiceOver'
- def speak(self, text, interrupt=0):
-  if interrupt:
-   self.runAppleScript('output ""')
-  thread = threading.Thread(target = self.speak_threaded, kwargs = {'text': text})
-  thread.start()
-  thread.join()
- def speak_threaded(self, text = ''):
-  self.runAppleScript('output "' + text + '"')
- def silence (self):
-  self.runAppleScript('output ""')
- def is_active(self):
-  return self.runAppleScript('return (name of processes) contains "VoiceOver"', 'system events').startswith('true') and not self.runAppleScript('try\nreturn bounds of vo cursor\non error\nreturn false\nend try').startswith('false')
-
-output_class = VoiceOver
+import subprocess, osfrom base import Outputclass VoiceOver(Output): """Speech output supporting the Apple VoiceOver screen reader.""" def runAppleScript(self, command, process = 'voiceover'):  return subprocess.Popen(['osascript', '-e', 'tell application "' + process + '"\n' + command + '\nend tell'], stdout = subprocess.PIPE).communicate()[0] name = 'VoiceOver' def speak(self, text, interrupt=0):  if interrupt:   self.silence()  os.system('osascript -e \"tell application \\\"voiceover\\\" to output \\\"%s\\\"\" &' % text) def silence (self):  self.runAppleScript('output ""') def is_active(self):  return self.runAppleScript('return (name of processes) contains "VoiceOver"', 'system events').startswith('true') and not self.runAppleScript('try\nreturn bounds of vo cursor\non error\nreturn false\nend try').startswith('false')output_class = VoiceOver
