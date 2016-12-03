d0e3f9919042151fe141601f9623d5ee6433bc17
Q <q@q-continuum.net>
2016-12-03 00:27:49 -0700

    Convert to tab-based indentation

--- accessible_output2/outputs/window_eyes.py
+++ accessible_output2/outputs/window_eyes.py
@@ -5,29 +5,29 @@ from .base import Output, OutputError
 import pywintypes
 
 class WindowEyes (Output):
- """Speech output supporting the WindowEyes screen reader"""
+	"""Speech output supporting the WindowEyes screen reader"""
 
- name = 'Window-Eyes'
+	name = 'Window-Eyes'
 
- def __init__(self, *args, **kwargs):
-  super(WindowEyes, self).__init__(*args, **kwargs)
-  try:
-   self.object = load_com("gwspeak.speak")
-  except pywintypes.com_error:
-   raise OutputError
+	def __init__(self, *args, **kwargs):
+		super(WindowEyes, self).__init__(*args, **kwargs)
+		try:
+			self.object = load_com("gwspeak.speak")
+		except pywintypes.com_error:
+			raise OutputError
 
- def speak(self, text, interrupt=0):
-  if interrupt:
-   self.silence()
-  self.object.SpeakString(text)
+	def speak(self, text, interrupt=0):
+		if interrupt:
+			self.silence()
+		self.object.SpeakString(text)
 
- def silence (self):
-  self.object.Silence()
+	def silence (self):
+		self.object.Silence()
 
- def is_active(self):
-  try:
-   return win32gui.FindWindow("GWMExternalControl", "External Control") != 0
-  except:
-   return False
+	def is_active(self):
+		try:
+			return win32gui.FindWindow("GWMExternalControl", "External Control") != 0
+		except:
+			return False
 
 output_class = WindowEyes 
