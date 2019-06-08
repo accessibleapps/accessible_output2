8504ef206849860a9f2805d68e7bc40cea8f2b0f
Carter Temm <crtbraille@gmail.com>
2019-06-08 14:55:57 -0500

    Ran black code formatter

--- accessible_output2/outputs/nvda.py
+++ accessible_output2/outputs/nvda.py
@@ -7,31 +7,34 @@ from platform_utils import paths
 from libloader import load_library
 from .base import Output
 
+
 class NVDA(Output):
-	"""Supports The NVDA screen reader"""
-	name = "NVDA"
-	lib32 = 'nvdaControllerClient32.dll'
-	lib64 = 'nvdaControllerClient64.dll'
-	argtypes = {
-		'nvdaController_brailleMessage': (ctypes.c_wchar_p,),
-	'nvdaController_speakText': (ctypes.c_wchar_p,),
-	}
+    """Supports The NVDA screen reader"""
 
-	def is_active(self):
-		try:
-			return self.lib.nvdaController_testIfRunning() == 0
-		except:
-			return False
+    name = "NVDA"
+    lib32 = "nvdaControllerClient32.dll"
+    lib64 = "nvdaControllerClient64.dll"
+    argtypes = {
+        "nvdaController_brailleMessage": (ctypes.c_wchar_p,),
+        "nvdaController_speakText": (ctypes.c_wchar_p,),
+    }
 
-	def braille(self, text, **options):
-		self.lib.nvdaController_brailleMessage(text)
+    def is_active(self):
+        try:
+            return self.lib.nvdaController_testIfRunning() == 0
+        except:
+            return False
 
-	def speak(self, text, interrupt=False):
-		if interrupt:
-			self.silence()
-		self.lib.nvdaController_speakText(text)
+    def braille(self, text, **options):
+        self.lib.nvdaController_brailleMessage(text)
 
-	def silence(self):
-		self.lib.nvdaController_cancelSpeech()
+    def speak(self, text, interrupt=False):
+        if interrupt:
+            self.silence()
+        self.lib.nvdaController_speakText(text)
+
+    def silence(self):
+        self.lib.nvdaController_cancelSpeech()
+
 
 output_class = NVDA
