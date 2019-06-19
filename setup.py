e0f33cfc4e6370c6f5412d5ab94b4958d89101c6
Christopher Toth <q.alpha@gmail.com>
2019-06-19 13:16:20 -0600

    Setup improvements including universal wheel and new-style platform-specific install requirements

--- setup.py
+++ setup.py
@@ -1,44 +1,36 @@
 from setuptools import setup, find_packages
 import io
-from platform import system
 
-_system = system()
+__doc__ = (
+    "Library to provide speech and braille output to a variety of different screen readers and other accessibility solutions.",
+)
 
-install_requires = ['platform_utils']
-if _system == 'Windows':
-	install_requires += [
-	'pypiwin32',
-	'libloader'
-]
-elif _system == 'Darwin':
-	install_requires += [
-		'appscript',
-	]
-
-__doc__ = 'Library to provide speech and braille output to a variety of different screen readers and other accessibility solutions.',
-
-with io.open('readme.rst', encoding='UTF8') as readme:
-	long_description = readme.read()
+with io.open("readme.rst", encoding="UTF8") as readme:
+    long_description = readme.read()
 
 setup(
-	name = 'accessible_output2',
-	author = 'Tyler Spivey',
-	author_email = 'tspivey@pcdesk.net',
-	version='0.13',
-	description = __doc__,
-	long_description = long_description,
-	package_dir = {'accessible_output2': 'accessible_output2'},
-	packages = find_packages(),
-	package_data = {"accessible_output2": ["lib/*"]},
-	zip_safe = False,
-	classifiers = [
-		'Development Status :: 5 - Stable',
-		'Intended Audience :: Developers',
-		'Operating System :: Microsoft :: Windows',
-		'Programming Language :: Python',
-		'License :: OSI Approved :: MIT License',
-'Topic :: Adaptive Technologies',
-'Topic :: Software Development :: Libraries'
-],
-	install_requires = install_requires,
+    name="accessible_output2",
+    author="Tyler Spivey",
+    author_email="tspivey@pcdesk.net",
+    version="0.14",
+    description=__doc__,
+    long_description=long_description,
+    package_dir={"accessible_output2": "accessible_output2"},
+    packages=find_packages(),
+    package_data={"accessible_output2": ["lib/*"]},
+    zip_safe=False,
+    install_requires=["platform_utils"],
+    extras_require={
+        ':sys_platform == "win32"': ["pywin32", "libloader"],
+        ':sys_platform == "darwin"': ["appscript"],
+    },
+    classifiers=[
+        "Development Status :: 5 - Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Adaptive Technologies",
+        "Topic :: Software Development :: Libraries",
+    ],
 )
